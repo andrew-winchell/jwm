99df99caba9f166addbf2ffc88173340261d2cf3
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-12-29 16:06:19 -0600

    Fix

--- js/layout-control.js
+++ js/layout-control.js
@@ -1,4 +1,4 @@
-const sidebar = $('.sidebar');
+const sidebar = document.querySelector('.sidebar');
 const mainContent = document.querySelector('.main-content');
 document.querySelector('#sidebar-btn').onclick = function () {
   sidebar.classList.toggle('sidebar_small');
