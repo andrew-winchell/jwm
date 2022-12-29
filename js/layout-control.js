38f7a445cc357dab93c6da625e6263950eb8ccc4
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-12-29 17:01:54 -0600

    Change tabs to buttons

--- js/layout-control.js
+++ js/layout-control.js
@@ -3,14 +3,3 @@ $("#sidebar-btn").click(() => {
     $(".main-content").toggleClass("main-content_large");
     $(".nav-item").toggleClass("nav-item_hide");
 });
-
-/*
-const sidebar = document.querySelector('.sidebar');
-const mainContent = document.querySelector('.main-content');
-const tabs = document.querySelector('.nav-item');
-document.querySelector('#sidebar-btn').onclick = function () {
-    sidebar.classList.toggle('sidebar_small');
-    mainContent.classList.toggle('main-content_large');
-    tabs.classList.toggle('nav-item_hide')
-}
-*/
