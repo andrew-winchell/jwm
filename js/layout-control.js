0d851c4fee2353a13f7dc24402da53a40997d642
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-12-29 16:16:03 -0600

    Hide tabs on sidebar collapse

--- js/layout-control.js
+++ js/layout-control.js
@@ -1,6 +1,8 @@
 const sidebar = document.querySelector('.sidebar');
 const mainContent = document.querySelector('.main-content');
+const tabs = document.querySelector('.nav-item');
 document.querySelector('#sidebar-btn').onclick = function () {
-  sidebar.classList.toggle('sidebar_small');
-  mainContent.classList.toggle('main-content_large')
+    sidebar.classList.toggle('sidebar_small');
+    mainContent.classList.toggle('main-content_large');
+    tabs.classList.toggle('nav-item_hide')
 }
