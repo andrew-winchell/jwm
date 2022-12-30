25dfb0455a3e3ca26ed5b0416f5354d78543f952
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-12-29 20:04:48 -0600

    Control panels with tabs

--- js/layout-control.js
+++ js/layout-control.js
@@ -4,3 +4,31 @@ $("#sidebar-btn").click(() => {
     $(".nav-item").toggleClass("nav-item_hide");
     $(".tabs").toggleClass("tabs_hide")
 });
+
+$("#data-tab").click(() => {
+    $("#home-panel").css("display", "block");
+    $("#event-panel").css("display", "none");
+    $("#iwa-panel").css("display", "none");
+    $("#report-panel").css("display", "none");
+});
+
+$("#event-tab").click(() => {
+    $("#home-panel").css("display", "none");
+    $("#event-panel").css("display", "block");
+    $("#iwa-panel").css("display", "none");
+    $("#report-panel").css("display", "none");
+});
+
+$("#iwa-tab").click(() => {
+    $("#home-panel").css("display", "none");
+    $("#event-panel").css("display", "none");
+    $("#iwa-panel").css("display", "block");
+    $("#report-panel").css("display", "none");
+});
+
+$("#report-tab").click(() => {
+    $("#home-panel").css("display", "none");
+    $("#event-panel").css("display", "none");
+    $("#iwa-panel").css("display", "none");
+    $("#report-panel").css("display", "block");
+});
