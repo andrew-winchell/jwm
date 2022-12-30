1dc808207a909ae2d4ba4315d1f3ea3bc548df87
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-12-29 21:13:55 -0600

    Active button

--- js/layout-control.js
+++ js/layout-control.js
@@ -5,20 +5,51 @@ $("#sidebar-btn").click(() => {
     $(".tabs").toggleClass("tabs_hide")
 });
 
-$(".tabs button").click((e) => {
-    let arr = ["#data-panel", "#event-panel", "#iwa-panel", "#report-panel"];
-    console.log(e.target.id, e.target.id.split("-"))
-    let category = e.target.id.split("-")[0];
-    let panel = "#" + category + "-panel";
-    let index = arr.indexOf(panel);
-    console.log(category, panel, index);
-    arr.splice(index, 1);
+$("#data-tab").click(() => {
+    // Panel visibility
+    $("#data-panel").css("display", "block");
+    $("#event-panel").css("display", "none");
+    $("#iwa-panel").css("display", "none");
+    $("#report-panel").css("display", "none");
+    // Tab appearance
+    $("#data-tab").addClass("tab-active");
+    $("#event-tab").removeClass("tab-active");
+    $("#iwa-tab").removeClass("tab-active");
+    $("#report-tab").removeClass("tab-active");
+});
 
-    $(panel).css("display", "block");
+$("#event-tab").click(() => {
+    $("#data-panel").css("display", "none");
+    $("#event-panel").css("display", "block");
+    $("#iwa-panel").css("display", "none");
+    $("#report-panel").css("display", "none");
+    // Tab appearance
+    $("#data-tab").removeClass("tab-active");
+    $("#event-tab").addClass("tab-active");
+    $("#iwa-tab").removeClass("tab-active");
+    $("#report-tab").removeClass("tab-active");
+});
 
-    for(let a of arr) {
-        console.log(a);
-        $(a).css("display", "none");
-    };
-    
+$("#iwa-tab").click(() => {
+    $("#data-panel").css("display", "none");
+    $("#event-panel").css("display", "none");
+    $("#iwa-panel").css("display", "block");
+    $("#report-panel").css("display", "none");
+    // Tab appearance
+    $("#data-tab").removeClass("tab-active");
+    $("#event-tab").removeClass("tab-active");
+    $("#iwa-tab").addClass("tab-active");
+    $("#report-tab").removeClass("tab-active");
+});
+
+$("#report-tab").click(() => {
+    $("#home-panel").css("display", "none");
+    $("#event-panel").css("display", "none");
+    $("#iwa-panel").css("display", "none");
+    $("#report-panel").css("display", "block");
+    // Tab appearance
+    $("#data-tab").removeClass("tab-active");
+    $("#event-tab").removeClass("tab-active");
+    $("#iwa-tab").removeClass("tab-active");
+    $("#report-tab").addClass("tab-active");
 });
