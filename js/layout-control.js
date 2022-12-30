de3ed3eeeb3e269d2ebda8ea47ca5bc5ebefe806
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2022-12-29 20:53:53 -0600

    Update layout-control.js

--- js/layout-control.js
+++ js/layout-control.js
@@ -12,58 +12,11 @@ $(".tabs button").click((e) => {
     let index = arr.indexOf(panel);
     arr.splice(index, 1);
 
+    $(panel).css("display", "block");
+
     for(let a of arr) {
         console.log(a);
         $(a).css("display", "none");
     };
     
-});
-
-$("#data-tab").click(() => {
-    // Panel visibility
-    $("#data-panel").css("display", "block");
-    $("#event-panel").css("display", "none");
-    $("#iwa-panel").css("display", "none");
-    $("#report-panel").css("display", "none");
-    // Tab appearance
-    $("#data-tab").addClass("tab-active");
-    $("#event-tab").removeClass("tab-active");
-    $("#iwa-tab").removeClass("tab-active");
-    $("#report-tab").removeClass("tab-active");
-});
-
-$("#event-tab").click(() => {
-    $("#data-panel").css("display", "none");
-    $("#event-panel").css("display", "block");
-    $("#iwa-panel").css("display", "none");
-    $("#report-panel").css("display", "none");
-    // Tab appearance
-    $("#data-tab").removeClass("tab-active");
-    $("#event-tab").addClass("tab-active");
-    $("#iwa-tab").removeClass("tab-active");
-    $("#report-tab").removeClass("tab-active");
-});
-
-$("#iwa-tab").click(() => {
-    $("#data-panel").css("display", "none");
-    $("#event-panel").css("display", "none");
-    $("#iwa-panel").css("display", "block");
-    $("#report-panel").css("display", "none");
-    // Tab appearance
-    $("#data-tab").removeClass("tab-active");
-    $("#event-tab").removeClass("tab-active");
-    $("#iwa-tab").addClass("tab-active");
-    $("#report-tab").removeClass("tab-active");
-});
-
-$("#report-tab").click(() => {
-    $("#home-panel").css("display", "none");
-    $("#event-panel").css("display", "none");
-    $("#iwa-panel").css("display", "none");
-    $("#report-panel").css("display", "block");
-    // Tab appearance
-    $("#data-tab").removeClass("tab-active");
-    $("#event-tab").removeClass("tab-active");
-    $("#iwa-tab").removeClass("tab-active");
-    $("#report-tab").addClass("tab-active");
 });
