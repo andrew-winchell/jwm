245a4981543502958cc3250542a9c629875731c8
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2023-01-04 22:02:30 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -330,6 +330,7 @@ require([
                 if (res.features.length > 0) {
                     editFeature = res.features[0];
                     eventForm.feature = editFeature;
+                    console.log(editFeature.event_name)
                     $("#weather-dropdown").val(editFeature.event_name).change();
                 }
             });
